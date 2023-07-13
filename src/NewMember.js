92f3f99dc5470d4c03f9ea8e3997fe17a953c953
Matt <mfouquier1@gmail.com>
2023-07-13 13:56:12 -0400

    created new user form html

--- src/NewMember.js
+++ src/NewMember.js
@@ -1,12 +1,52 @@
 import React from "react";
 import { NavBar } from "./NavBar";
+import { Select, FormControl, InputLabel, Input, MenuItem } from "@mui/material";
+import './styles/NewMember.css'
+
+
 
 export const NewMember = () => {
 
   return (
     <>
       <NavBar />
-      <h3>New Member Page</h3>
+      <div className="newuser-form">
+        <div className="row1">
+          <FormControl id="barcodeBox">
+            <InputLabel>Barcode</InputLabel>
+            <Input id="barcode" aria-describedby="barcode #"></Input>
+          </FormControl>
+          <FormControl id="fnameBox">
+            <InputLabel>First Name</InputLabel>
+            <Input id="fname" aria-describedby="fname"></Input>
+          </FormControl>
+          <FormControl id="lnameBox">
+            <InputLabel>Last Name</InputLabel>
+            <Input id="lname" aria-describedby="lname"></Input>
+          </FormControl>
+        </div>
+        <div className="row2">
+          <FormControl id="dodidBox">
+            <InputLabel>DoD ID</InputLabel>
+            <Input id="dodid" aria-describedby="dodid"></Input>
+          </FormControl>
+          <FormControl id="directorateBox">
+            <InputLabel>Directorate</InputLabel>
+            <Input id="directorate" aria-describedby="directorate"></Input>
+          </FormControl>
+          <FormControl id="orgBox">
+            <InputLabel>Organization</InputLabel>
+            <Select name='org' id="org">
+              <MenuItem key="soccent">SOCCENT</MenuItem>
+              <MenuItem key="socom">SOCOM</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+
+      </div>
+
+
 
     </>
 
